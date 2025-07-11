6b60a874f8de75ea2f576b2a6d2abd1bbceccd87
avenir-crm <avenirindianks@gmail.com>
2025-07-12 02:46:40 +0530

    baba

--- kda/modules/employees/employee_add.php
+++ kda/modules/employees/employee_add.php
@@ -30,7 +30,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html>
-<head>
+<head> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <title>Add Employee</title>
     <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
 </head>
