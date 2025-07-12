4178b1d52dc50f4d87bf634788aa674270ba32f0
avenir-crm <avenirindianks@gmail.com>
2025-07-12 21:59:20 +0530

    baba

--- kda/config/db.php
+++ kda/config/db.php
@@ -1,15 +1,4 @@
 <?php
-session_start();
-include($_SERVER['DOCUMENT_ROOT'].'/Project/kda/config/db.php');
-
-// তারপর তোমার query
-$today = date('j');
-$query = "SELECT * FROM branches WHERE rent_payment_date = $today";
-$result = mysqli_query($conn, $query);
-
-// etc...
-?>
-<?php
 $servername = "localhost";
 $username   = "root";
 $password   = "";
