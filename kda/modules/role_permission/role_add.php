14bf4a8f6a3e59f5e7444f465ed14f64723a2f2b
avenir-crm <avenirindianks@gmail.com>
2025-07-12 15:17:53 +0530

    maa

--- kda/modules/role_permission/role_add.php
+++ kda/modules/role_permission/role_add.php
@@ -2,6 +2,10 @@
 include('../../config/constants.php');
 include(BASE_PATH . '/config/db_connect.php');
 
+if(!isset($_POST['designation_name']) || empty($_POST['designation_name'])){
+  die("Designation Name missing.");
+}
+
 $designation_name = mysqli_real_escape_string($conn, $_POST['designation_name']);
 
 mysqli_query($conn, "INSERT INTO designations (designation_name) VALUES ('$designation_name')");
@@ -9,5 +13,4 @@ mysqli_query($conn, "INSERT INTO designations (designation_name) VALUES ('$desig
 header("Location: role_list.php?msg=Designation added successfully");
 exit;
 ?>
-
-<?php include('../../includes/footer.php'); ?>
+<?php include 'admin_footer.php'; ?>
