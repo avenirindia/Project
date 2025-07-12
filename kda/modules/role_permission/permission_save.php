61a35626a7363be6c0e262f7381f660be7a16adb
avenir-crm <avenirindianks@gmail.com>
2025-07-12 14:23:33 +0530

    baba

--- kda/modules/role_permission/permission_save.php
+++ kda/modules/role_permission/permission_save.php
@@ -1,5 +1,5 @@
 <?php
-include('../../config/db.php');
+include('../../../config/db_connect.php');
 
 $designation_id = $_POST['designation_id'];
 $permissions = isset($_POST['permissions']) ? $_POST['permissions'] : [];
