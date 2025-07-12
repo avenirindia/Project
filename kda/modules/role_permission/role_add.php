85b638e622bd2a9ae2609deae5d9895009f982b8
avenir-crm <avenirindianks@gmail.com>
2025-07-12 15:14:59 +0530

    maa

--- kda/modules/role_permission/role_add.php
+++ kda/modules/role_permission/role_add.php
@@ -1,16 +1,13 @@
 <?php
 include('../../config/constants.php');
 include(BASE_PATH . '/config/db_connect.php');
+
+$designation_name = mysqli_real_escape_string($conn, $_POST['designation_name']);
+
+mysqli_query($conn, "INSERT INTO designations (designation_name) VALUES ('$designation_name')");
+
+header("Location: role_list.php?msg=Designation added successfully");
+exit;
 ?>
-<div class="container mt-4">
-  <h4 class="mb-4">Add New Designation</h4>
-  <form method="post" action="role_add_save.php">
-    <div class="mb-3">
-      <label class="form-label">Designation Name</label>
-      <input type="text" name="designation_name" class="form-control" placeholder="Enter Designation Name" required>
-    </div>
-    <button type="submit" class="btn btn-primary">Add Designation</button>
-    <a href="role_list.php" class="btn btn-secondary">Back to List</a>
-  </form>
-</div>
+
 <?php include('../../includes/footer.php'); ?>
