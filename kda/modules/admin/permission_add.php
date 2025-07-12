3594468ba9203b570229b689fd502f1c3faaa934
avenir-crm <avenirindianks@gmail.com>
2025-07-12 17:13:55 +0530

    baba

--- kda/modules/admin/permission_add.php
+++ kda/modules/admin/permission_add.php
@@ -1,4 +1,14 @@
 <?php
+include($_SERVER['DOCUMENT_ROOT'].'/Project/kda/config/db.php');
+?>
+<!DOCTYPE html>
+<html>
+<head><title>Add Employee</title></head>
+<body>
+<h2>Add Employee</h2>
+</body>
+</html>
+<?php
 include '../../config/db_connect.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST['permission_name'];
