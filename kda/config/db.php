3e04286e592405d481548a2061645f7d942d5e25
avenir-crm <avenirindianks@gmail.com>
2025-07-12 21:53:48 +0530

    baba

--- kda/config/db.php
+++ kda/config/db.php
@@ -1,11 +1,23 @@
 <?php
+session_start();
+include($_SERVER['DOCUMENT_ROOT'].'/Project/kda/config/db.php');
+
+// তারপর তোমার query
+$today = date('j');
+$query = "SELECT * FROM branches WHERE rent_payment_date = $today";
+$result = mysqli_query($conn, $query);
+
+// etc...
+?>
+<?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "project";
+$username   = "root";
+$password   = "";
+$dbname     = "project";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Database connection failed: " . mysqli_connect_error());
 }
 ?>
