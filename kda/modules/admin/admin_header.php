2294072bcdb8da35341a1331ec2d1cf0998b960c
avenir-crm <avenirindianks@gmail.com>
2025-07-12 12:39:47 +0530

    baba

---
+++ kda/modules/admin/admin_header.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="../dashboard.php">KDA Microfinance ERP</a>
+  </div>
+</nav>
+
+<div class="d-flex">
+  <div class="bg-light border" style="width:250px; height:100vh; padding:20px;">
+    <h6>📁 Modules</h6>
+    <ul class="nav flex-column">
+      <li class="nav-item"><a class="nav-link" href="../dashboard.php">🏠 Dashboard</a></li>
+      <li class="nav-item"><a class="nav-link" href="../modules/admin/designation_list.php">👥 Designations</a></li>
+      <li class="nav-item"><a class="nav-link" href="../modules/employees/employee_list.php">🧑‍💼 Employees</a></li>
+      <li class="nav-item"><a class="nav-link" href="../modules/branches/branch_list.php">🏢 Branches</a></li>
+      <li class="nav-item"><a class="nav-link" href="../modules/loans/loan_list.php">💳 Loans</a></li>
+      <li class="nav-item"><a class="nav-link" href="../modules/accounts/account_list.php">📊 Accounts</a></li>
+      <li class="nav-item"><a class="nav-link" href="../modules/admin/permission_list.php">🛡️ Permissions</a></li>
+    </ul>
+  </div>
+
+  <div class="p-4" style="flex-grow:1;">
---
+++ kda/modules/admin/dashboard.php
@@ -0,0 +1,20 @@
+<?php include 'modules/admin/admin_header.php'; ?>
+
+<h3>📊 Welcome to KDA Microfinance ERP Dashboard</h3>
+
+<div class="row mt-4">
+  <div class="col-md-3">
+    <div class="card text-bg-primary p-3">
+      <h5>Total Branches</h5>
+      <p>5</p>
+    </div>
+  </div>
+  <div class="col-md-3">
+    <div class="card text-bg-success p-3">
+      <h5>Total Employees</h5>
+      <p>12</p>
+    </div>
+  </div>
+</div>
+
+<?php include 'modules/admin/admin_footer.php'; ?>
