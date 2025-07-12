331c12fa02615fa0cb9dc9d94f261fbcaceec89b
avenir-crm <avenirindianks@gmail.com>
2025-07-12 19:40:39 +0530

    baba

--- kda/modules/employees/designation_add.php
+++ kda/modules/employees/designation_add.php
@@ -1,4 +1,14 @@
 <?php
+if(isset($_SESSION['error'])) {
+  echo "<div class='alert alert-danger'>".$_SESSION['error']."</div>";
+  unset($_SESSION['error']);
+}
+if(isset($_SESSION['success'])) {
+  echo "<div class='alert alert-success'>".$_SESSION['success']."</div>";
+  unset($_SESSION['success']);
+}
+?>
+<?php
 session_start();
 include($_SERVER['DOCUMENT_ROOT'].'/Project/kda/config/db.php');
 
